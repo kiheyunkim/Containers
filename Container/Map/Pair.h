f35a6f67ee37d39f22ebcd90264b444cf7015ac1
kiheyunkim <kiheyunKim@gmail.com>
2018-02-06 09:50:38 +0900

    Add Map
    Add Map Solution And make Pair for Map Container

---
+++ Container/Map/Pair.h
@@ -0,0 +1,33 @@
+#ifndef _PAIR_H_
+#define _PAIR_H_
+
+template<typename First, typename Second>
+class Pair
+{
+public:
+	First first;
+	Second second;
+
+public:
+	Pair& operator=(const Pair<First, Second>& right)
+	{
+		first = right->first;
+		second = right->second;
+	}
+
+
+	void swap(Pair<First, Second>& right)
+	{
+		First tempFirst = right.first;
+		Second tempSecond = right.second;
+
+		right.first = first;
+		right.second = second;
+
+		first = tempFirst;
+		second = tempSecond;
+	}
+
+};
+
+#endif
